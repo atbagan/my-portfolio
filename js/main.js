6cf259e7e9292f93f7b25788a59b1ca04c335c46
Andrew Bagan <andrewbagan2212@gmail.com>
2020-05-20 16:07:37 -0500

    changed index and main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,34 @@
-import 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ExampleWork from './example-work';
 
-console.log("loaded react-dom");
+const myWork = [
+    {
+        'title': "Serverless Attendance App",
+        'image': {
+            'desc': "example screenshot of a project involving code",
+            'src': "images/example1.png",
+            'comment': ""
+        }
+    },
 
-console.log("Webpack Works");
+    {
+        'title': "Portfolio Boilerplate",
+        'image': {
+            'desc': "A Serverless Portfolio",
+            'src': "images/example2.png",
+            'comment': ""
+        }
+    },
+
+    {
+        'title': "Work Example",
+        'image': {
+            'desc': "example screenshot of a project involving cats",
+            'src': "images/example3.png",
+            'comment': ""
+        }
+    },
+]
+
+ReactDOM.render(<ExampleWork work={myWork}/>, document.getElementById('example-work'));
