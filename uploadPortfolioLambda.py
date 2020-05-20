9ec0894d6ac5011919d649e12e562d0ac0cb3aab
Andrew Bagan <andrewbagan2212@gmail.com>
2020-05-20 00:11:20 -0500

    Updating lambda

--- uploadPortfolioLambda.py
+++ uploadPortfolioLambda.py
@@ -5,20 +5,28 @@ import mimetypes
 from io import BytesIO
 from botocore.client import Config
 
-
 def lambda_handler(event, context):
-    s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
 
-    portfolio_bucket = s3.Bucket('portfolio.andrewbagan.io')
-    build_bucket = s3.Bucket('portfoliobuild.andrewbagan.io')
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:us-east-1:394138567482:deployPortfolioTopic')
 
-    portfolio_zip = BytesIO()
+    try:
+        s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
 
-    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+        portfolio_bucket = s3.Bucket('portfolio.andrewbagan.io')
+        build_bucket = s3.Bucket('portfoliobuild.andrewbagan.io')
 
-    with zipfile.ZipFile(portfolio_zip) as myzip:
-        for name in myzip.namelist():
-            obj = myzip.open(name)
-            portfolio_bucket.upload_fileobj(obj, name)
-            portfolio_bucket.Object(name).Acl().put(ACL='public-read')
+        portfolio_zip = BytesIO()
+
+        build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+            for name in myzip.namelist():
+                obj = myzip.open(name)
+                portfolio_bucket.upload_fileobj(obj,name)
+                portfolio_bucket.Object(name).Acl().put(ACL='public-read')
+        topic.publish(Subject="Portfolio Deployed", Message="Portfolio Deployed Successfully!")
+    except:
+         topic.publish(Subject="Portfolio Deploy Failed", Message="The Portfolio was not deployed successfully!")
+         raise
     return None
